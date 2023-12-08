e7ac35b06cc8d7372f45b941de5eca9f02eb54a9
David Raška <jeffraska@users.noreply.github.com>
2023-12-08 14:24:09 +0100

    Fix compatibility issue with MySql.Data >=6.10.0

--- Utf8mb3.cs
+++ Utf8mb3.cs
@@ -7,11 +7,18 @@ namespace Jf.MySql.Data.Collations
 	// ReSharper disable once InconsistentNaming
 	public static class Utf8mb3
 	{
+		private static readonly Version NewFieldNamingVersion = new(6, 10, 0);
+		
 		public static void Enable()
 		{
 			// Add internal mapping of database utf8mb3 charset to .NET framework's UTF-8 encoding
-			var mappingField = System.Reflection.Assembly.GetAssembly(typeof(MySqlConnection))
-				.GetType("MySql.Data.MySqlClient.CharSetMap").GetField("mapping",
+			var assembly = System.Reflection.Assembly.GetAssembly(typeof(MySqlConnection));
+			var connectorVersion = assembly.GetName().Version;
+			
+			var mappingFieldName = connectorVersion >= NewFieldNamingVersion ? "_mapping" : "mapping";
+			
+			var mappingField = assembly
+				.GetType("MySql.Data.MySqlClient.CharSetMap").GetField(mappingFieldName,
 					System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.GetField |
 					System.Reflection.BindingFlags.Static);
 
